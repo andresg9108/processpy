b47a3843fe2a564a9d4236a6c3b57cad91f3adba
Andres Gonzalez <andresg9108@gmail.com>
2019-07-11 17:42:46 -0500

    Module html

--- process.py
+++ process.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 import sys
-from settings import useful
+from modules import useful
+from modules import html
+
+sTemplateRoute = './pageTemplates'
+sInitialRoute = './pages'
+sFinalRoute = './web'
+sFileLog = './manyp.log'
+aIgnore = ['src']
 
 sArgv = sys.argv[1]
 
 if sArgv == '-test':
 	useful.test()
+elif sArgv == '-l':
+	html.deleteFilesOrDirectories(sFinalRoute, aIgnore)
+	html.loadPagefiles(sTemplateRoute, sInitialRoute, sFinalRoute)
+	html.loadLogs(sFileLog)
+
+	print('OK')
 else:
 	print('Incorrect command')
